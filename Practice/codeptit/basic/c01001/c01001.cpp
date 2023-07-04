e902a629e4d00bf0ecfa3935f4d9a2f1d0c82891
Duong Vu Cong Tuan <duongvct@duongvct>
2023-07-04 11:24:26 +0700

    do first 3 exercises in 3 different languages

---
+++ Practice/codeptit/basic/c01001/c01001.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main () {
+    int n;
+    std::cin>>n;
+    long long res = n * 2;
+    std::cout << res;
+}
---
+++ Practice/codeptit/basic/c01003/c01003.py
@@ -0,0 +1,5 @@
+test = int(input())
+while (test > 0) :
+    n = int(input())
+    print(n ** 2)
+    test -= 1
